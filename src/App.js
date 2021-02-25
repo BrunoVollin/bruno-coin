6e2a53de9d344541b2e03f3e679f91bdf019f0cc
bruno <bruno.3av@gmail.com>
2021-02-25 17:36:44 -0300

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const [cryptoName, setCryptoName] = useState();
+  const [cryptoInfo, setCryptoInfo] = useState("nada");
+
+  function handleChange(event) {
+    setCryptoName(event.target.value);
+  }
+
+  // useEffect(() => {
+  //   axios
+  //     .get(`https://www.mercadobitcoin.net/api/${cryptoName}/ticker/`)
+  //     .then((res) => {
+  //       const infos = res.data;
+  //       setCryptoInfo(infos);
+  //     });
+  // }, [cryptoName]);
+
+  //TEST
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <label>
+        <select value={cryptoName} onChange={handleChange}>
+          <option value="BTC">Bitcoin</option>
+          <option value="kkk">kkk</option>
+          <option value="coco">Coco</option>
+          <option value="manga">Manga</option>
+        </select>
+      </label>
+      <br />
+      <button
+        onClick={() => {
+          setCryptoInfo({
+            ticker: {
+              high: 14481.47,
+              low: 13706.00002,
+              vol: 443.73564488,
+              last: 14447.01,
+              buy: 14447.001,
+              sell: 14447.01,
+              date: 1502977646,
+            },
+          });
+          console.log("o--->", cryptoInfo.ticker);
+        }}
+      >
+        oo
+      </button>
+      <br />
+
+      <p>high: </p>
+      <p></p>
+      <p></p>
+      <p></p>
+    </>
   );
 }
 
